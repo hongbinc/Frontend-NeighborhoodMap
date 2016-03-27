b26b7414106a24084a16048d772ac7ee2daa8dac
hongbinc <hongbin1992@hotmail.com>
2016-03-26 21:40:14 -0400

    Implement google maps API

---
+++ js/app.js
@@ -0,0 +1,30 @@
+﻿/**
+ * Main JS file for NeighborhoodMap Application
+ * Model and View Model
+ */
+
+var map;
+// initializing the Google Map
+function initializeMap() {
+
+    mapOptions = {
+        zoom: 15,
+        disableDefaultUI: true,
+        center: { lat: -34.397, lng: 150.644 },
+    };
+
+    /*
+    if (typeof google == 'undefined') {
+        $('#googleMap-API-error').html('<h2>There are errors when retrieving map data.</h2><h2>Please try refresh page later.</h2>');
+        return;
+    }
+    */
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $('#map').height($(window).height());
+
+};
+
+
+initializeMap();
